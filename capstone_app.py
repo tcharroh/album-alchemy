a293d372d2af2944bd0aa880c190dd45157f86c2
Kevin Borah <kevinborah@gmail.com>
2023-04-03 22:06:58 -0400

    key add

--- capstone_app.py
+++ capstone_app.py
@@ -60,6 +60,7 @@ create = st.button('**make album alchemy**')
 #######################################
 # Run model given the above selections#
 openai.api_type = "open_ai"
+openai.organization_key = 'org-0skHZAjWNB79DqtyyXTKGDGI'
 openai.api_key = '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'
 openai.api_base = "https://api.openai.com/v1"
 openai.api_version = None
