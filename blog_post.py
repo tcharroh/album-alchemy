a96196ceb31e181caf5c92e0241bc46f8cf9163a
Kevin Borah <kevinborah@gmail.com>
2023-04-14 14:43:03 -0400

    sidebar

--- blog_post.py
+++ blog_post.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="how we did it", page_icon=None)
+st.set_page_config(page_title="how_we_did_it", page_icon=None)
+
+st.sidebar.header("how we did it")
 st.title('how we made & evaluated album alchemy')
