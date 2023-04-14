0addf3521d7fd6381ad3c1ebbf3f3aacd1030461
Kevin Borah <kevinborah@gmail.com>
2023-04-14 15:01:19 -0400

    changes header

--- pages/blog_post.py
+++ pages/blog_post.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="how_we_did_it", page_icon=None)
 
-st.sidebar.header("how we did it")
+st.sidebar.header("album alchemy")
 st.title('how we made & evaluated album alchemy')
